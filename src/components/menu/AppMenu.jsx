cbf95a69de44f7a4edee29cddc84c36729816cc8
Lucas Rezende <lucasffrezende@gmail.com>
2024-08-07 10:26:41 -0300

    feat: add menu component

---
+++ src/components/menu/AppMenu.jsx
@@ -0,0 +1,21 @@
+import { MegaMenu } from "primereact/megamenu";
+import React from "react";
+import logo from "../../../src/assets/images/logo-horizontal.png";
+
+export default function AppMenu() {
+    const items = [
+        {
+            label: 'Empresas',
+            icon: 'pi pi-building',
+            url: '/empresas'
+        },
+    ];
+
+    const start = <a href="/"><img alt="logo" src={logo} height={60} className="mr-2" /></a>
+
+    return (
+        <div className="card">
+            <MegaMenu model={items} orientation="horizontal" start={start} breakpoint="600px" className="p-3 surface-0 shadow-2 mb-2" style={{ borderRadius: '1rem' }} />
+        </div>
+    )
+}
