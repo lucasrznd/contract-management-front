f7e0939387a4e1fad9478d28ab242f45ed66ecb8
Lucas Rezende <lucasffrezende@gmail.com>
2024-08-07 10:27:45 -0300

    feat: add routes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import 'primereact/resources/themes/lara-light-blue/theme.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
+import "primereact/resources/themes/lara-light-blue/theme.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import AppRoutes from "./routes/Routes";
 
 export default function App() {
   return (
     <div>
-
-    </div>  
+      <AppRoutes />
+    </div>
   );
 }
