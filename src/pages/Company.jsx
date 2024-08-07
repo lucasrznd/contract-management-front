03a4af182c12f71fe927e38b5781293ac3a96e42
Lucas Rezende <lucasffrezende@gmail.com>
2024-08-07 10:38:28 -0300

    build: add helmet provider library

--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -1,8 +1,15 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import AppFooter from "../components/footer/AppFooter";
 import AppMenu from "../components/menu/AppMenu";
 
 export default function CompanyPage() {
     return <div>
+        <HelmetProvider>
+            <Helmet>
+                <title>{"EducadoraGC - Empresas"}</title>
+            </Helmet>
+        </HelmetProvider>
+
         <AppMenu />
 
         <div className="flex align-items-center justify-content-center">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import AppFooter from "../components/footer/AppFooter";
 import AppMenu from "../components/menu/AppMenu";
 
 export default function HomePage() {
     return <div>
+        <HelmetProvider>
+            <Helmet>
+                <title>{"EducadoraGC - Home"}</title>
+            </Helmet>
+        </HelmetProvider>
+
         <AppMenu />
         <div className="flex align-items-center justify-content-center">
             <h1>Home</h1>
