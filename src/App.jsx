521d11c77181038f614030ce41b8c17f231fd723
Lucas Rezende <lucasffrezende@gmail.com>
2024-08-07 09:48:41 -0300

    chore: create project structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import 'primereact/resources/themes/lara-light-blue/theme.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+export default function App() {
+  return (
+    <div>
+
+    </div>  
+  );
+}
