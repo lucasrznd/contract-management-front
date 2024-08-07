61f6978baf77e2e61a29621e8560b6c3bcdc24f3
Lucas Rezende <lucasffrezende@gmail.com>
2024-08-07 10:26:18 -0300

    feat: add footer component

---
+++ src/components/footer/AppFooter.jsx
@@ -0,0 +1,7 @@
+export default function AppFooter() {
+    return (
+        <div className="flex align-items-center justify-content-center bg-primary border-round mt-2 h-4rem">
+            <p className="text-lg font-bold">EducadoraGC - Todos os direitos reservados.&copy;</p>
+        </div>
+    )
+}
