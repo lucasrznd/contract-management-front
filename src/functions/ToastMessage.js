cd4b48f4bf39a22a94ca65372ee29c9073c3bc3f
Lucas Rezende <lucasffrezende@gmail.com>
2024-08-15 09:38:23 -0300

    chore: change toast duration

--- src/functions/ToastMessage.js
+++ src/functions/ToastMessage.js
@@ -1,15 +1,15 @@
 export function successMsg(toast, msg) {
-    toast.current.show({ severity: 'success', summary: 'Sucesso', detail: msg, life: 3500 });
+    toast.current.show({ severity: 'success', summary: 'Sucesso', detail: msg, life: 5000 });
 }
 
 export function infoMsg(toast, msg) {
-    toast.current.show({ severity: 'info', summary: 'Info', detail: msg, life: 3500 });
+    toast.current.show({ severity: 'info', summary: 'Info', detail: msg, life: 5000 });
 }
 
 export function warnMsg(toast, msg) {
-    toast.current.show({ severity: 'warn', summary: 'Aviso', detail: msg, life: 3500 });
+    toast.current.show({ severity: 'warn', summary: 'Aviso', detail: msg, life: 5000 });
 }
 
 export function errorMsg(toast, msg) {
-    toast.current.show({ severity: 'error', summary: 'Erro', detail: msg, life: 3500 });
+    toast.current.show({ severity: 'error', summary: 'Erro', detail: msg, life: 5000 });
 }
