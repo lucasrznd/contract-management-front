4021c04796a952c6dec7f3d763bf01e1f9689da5
Lucas Rezende <lucasffrezende@gmail.com>
2024-08-17 10:34:33 -0300

    feat: add functions to format date

--- src/functions/StringUtils.js
+++ src/functions/StringUtils.js
@@ -35,3 +35,18 @@ export function buildEncodedStringAddress(rowData) {
 
     return encodedAddress;
 }
+
+export function dateFormatDt(rowData, columnName) {
+    if (rowData && rowData[columnName]) {
+        const [year, month, day] = rowData[columnName].split('-').map(Number);
+        const data = new Date(year, month - 1, day);
+        return data.toLocaleDateString('pt-BR');
+    }
+    return '';
+}
+
+export function parseDate(date) {
+    const [year, month, day] = date.split('-').map(Number);
+    // month is 0-based in JavaScript Date, so we subtract 1 from month
+    return new Date(year, month - 1, day);
+}
